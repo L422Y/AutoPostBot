579f5cf6a96969b9c8632d35dfe64a7e509e904d
Larry W <l422y@l422y.com>
2023-08-27 09:17:25 -0400

    Add BaseDestinationPlugin and BaseGeneratorPlugin, remove BaseTweetPlugin and TwitterPostBot

---
+++ lib/BaseDestinationPlugin.ts
@@ -0,0 +1,26 @@
+import { IDestinationPlugin } from "../types/IDestinationPlugin"
+
+export class BaseDestinationPlugin implements IDestinationPlugin {
+    name: string = "BasePostPlugin"
+
+    constructor(options?: any) {
+        this.log(`...Initialized`)
+    }
+
+    async initialize(): Promise<void> {
+        return Promise.resolve(undefined)
+    }
+
+    async sendPost(postContent: string): Promise<void> {
+        return Promise.resolve(undefined)
+    }
+
+    async confirmPostSent(): Promise<void> {
+        return Promise.resolve(undefined)
+    }
+
+    log(...args: any[]): void {
+        console.log(`[${this.name}]`, ...args)
+    }
+
+}
---
+++ lib/BaseGeneratorPlugin.ts
@@ -0,0 +1,25 @@
+import { IGeneratorPlugin } from "../types/IGeneratorPlugin"
+
+export class BaseGeneratorPlugin implements IGeneratorPlugin {
+    chance: number = 0.5
+    name: string = "BasePostPlugin"
+
+    constructor(options?: any) {
+        if (options?.chance) {
+            this.chance = options.chance
+        }
+        this.log(`...Initialized with chance: ${this.chance}`)
+    }
+
+    async generatePost(): Promise<string | void> {
+        return Promise.resolve(undefined)
+    }
+
+    async confirmPostSent(): Promise<void> {
+        return Promise.resolve(undefined)
+    }
+
+    log(...args: any[]): void {
+        console.log(`[${this.name}]`, ...args)
+    }
+}
